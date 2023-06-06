85ff0824c4eb32476b3c2a58572dfbe7c0726edf
Giovanni Buturi <123384250+gibneto@users.noreply.github.com>
2023-06-06 00:14:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import { 
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay
+ } from "./elements.js"
+
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
+})
+
+const sound = Sound()
+
+Events({controls, timer, sound})
